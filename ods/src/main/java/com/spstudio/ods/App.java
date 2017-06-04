9d132e15f0c954b03d2055a2b9a464f02cff779c
william <streetpoet@163.com>
2017-06-04 16:54:44 -0500

    initial files

---
+++ ods/src/main/java/com/spstudio/ods/App.java
@@ -0,0 +1,37 @@
+package com.spstudio.ods;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.TemplateHandler;
+import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
+
+public class App extends AbstractVerticle {
+
+	@Override
+	public void start(Future<Void> startFuture) throws Exception {
+		HttpServer server = vertx.createHttpServer();
+
+		Router router = Router.router(vertx);
+
+		StaticHandler staticHandler = StaticHandler.create();
+		staticHandler.setDefaultContentEncoding("utf-8");
+		router.route("/static/*").handler(staticHandler);
+		
+		router.route().handler(routingContext -> {
+			routingContext.put("welcome", "hello, streetpoet");
+			routingContext.next();
+		});
+
+		// template engine configuration
+		ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
+		TemplateHandler handler = TemplateHandler.create(engine);
+
+		router.get("/business/*").handler(handler);
+
+		server.requestHandler(router::accept).listen(8080);
+	}
+
+}
