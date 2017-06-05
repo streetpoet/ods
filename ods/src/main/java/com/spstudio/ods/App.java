5c0a2adccd2446f1f48f5f53145ea366c0deaf38
william <william.wang@praxent.com>
2017-06-04 23:02:35 -0500

    update

--- ods/src/main/java/com/spstudio/ods/App.java
+++ ods/src/main/java/com/spstudio/ods/App.java
@@ -1,8 +1,12 @@
 package com.spstudio.ods;
 
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.jdbc.JDBCAuth;
+import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.TemplateHandler;
@@ -12,6 +16,24 @@ public class App extends AbstractVerticle {
 
 	@Override
 	public void start(Future<Void> startFuture) throws Exception {
+		
+		JsonObject config = new JsonObject()
+				.put("driver_class", "com.mysql.jdbc.Driver")
+				.put("url", "localhost:3306")
+				.put("user", "vertx")
+				.put("password", "password")
+				.put("database", "testdb")
+				.put("charset", "utf-8");
+		JDBCClient client = JDBCClient.createShared(vertx, config);
+		JDBCAuth authProvider = JDBCAuth.create(vertx, client);
+		client.getConnection(res -> {
+			if (res.succeeded()) {
+				res.result().query("select 1 from duel", r -> {
+					System.out.println(r.result().getResults().get(0));
+				});
+			}
+		});
+		
 		HttpServer server = vertx.createHttpServer();
 
 		Router router = Router.router(vertx);
@@ -21,6 +43,9 @@ public class App extends AbstractVerticle {
 		router.route("/").handler(staticHandler);
 		router.route("/static/*").handler(staticHandler);
 		
+//		AuthHandler authHandler = buildJwtAuthHandler();
+//		router.route("/business/*").handler(authHandler);
+
 		router.route().handler(routingContext -> {
 			routingContext.put("welcome", "hello, streetpoet");
 			routingContext.next();
@@ -35,4 +60,12 @@ public class App extends AbstractVerticle {
 		server.requestHandler(router::accept).listen(8080);
 	}
 
+//	protected AuthHandler buildJwtAuthHandler() {
+//		JsonObject config = new JsonObject().put("keyStore",
+//				new JsonObject().put("path", "keystore.jceks").put("type", "jceks").put("password", "213231"));
+//		JWTAuth provider = JWTAuth.create(vertx, config);
+//		String token = provider.generateToken(new JsonObject().put("sub", "1"), new JWTOptions());
+//		System.out.println(String.format("token: %s", token));
+//		return JWTAuthHandler.create(provider);
+//	}
 }
