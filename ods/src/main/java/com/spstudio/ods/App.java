0e3ebb340f95d39dff34d83252a9fc82249e796e
William <williambetacat@gmail.com>
2017-06-06 13:06:47 -0500

    update

--- ods/src/main/java/com/spstudio/ods/App.java
+++ ods/src/main/java/com/spstudio/ods/App.java
@@ -39,53 +39,57 @@ public class App extends AbstractVerticle {
 		router.route().handler(UserSessionHandler.create(authProvider));
 		
 		AuthHandler authHandler = BasicAuthHandler.create(authProvider);
-
 		router.route("/business/*").handler(authHandler);
 		
-//		router.route("/gen").blockingHandler(routingContext -> {
-//			JsonObject config = new JsonObject() 
-//					.put("driver_class", "com.mysql.jdbc.Driver")
-//					.put("url", "jdbc:mysql://localhost/testdb")
-//					.put("user", "vertx")
-//					.put("password", "password");
-//			JDBCClient client = JDBCClient.createShared(vertx, config);
-//			JDBCAuth authProvider = JDBCAuth.create(vertx, client);
-//			String salt = authProvider.generateSalt();
-//			String hash = authProvider.computeHash("213231", salt);
-//			
-//			client.getConnection(res -> {
-//				if (res.succeeded()) {
-//					res.result().updateWithParams("INSERT INTO user VALUES (?, ?, ?)", new JsonArray().add("streetpoet").add(hash).add(salt), r -> {
-//						if (r.succeeded()) {
-//							System.out.println("insert success!");
-//						}else {
-//							System.out.println(r.cause().getMessage());
-//						}
-//						routingContext.response().end();
-//					});
-//				}
-//			});
-//		});
-
 		StaticHandler staticHandler = StaticHandler.create();
 		staticHandler.setDefaultContentEncoding("utf-8");
 		router.route("/").handler(staticHandler);
 		router.route("/static/*").handler(staticHandler);
-
-		// AuthHandler authHandler = buildJwtAuthHandler(); 
-		// router.route("/business/*").handler(authHandler);
-
-		router.route().handler(routingContext -> {
-			routingContext.put("welcome", "hello, streetpoet");
-			routingContext.next();
+		
+		router.route("/logout").handler(routingContext -> {
+			routingContext.clearUser();
+			routingContext.reroute("/");
+		});
+		
+		// Authorization
+		router.route("/business/label").handler(rc -> {
+			rc.user().isAuthorised("role:admin", handler -> {
+				if (handler.failed()) {
+					rc.reroute("/");
+				}
+			});
 		});
 
 		// template engine configuration
 		ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
 		TemplateHandler handler = TemplateHandler.create(engine); 
-
-		router.get("/business/*").handler(handler);
+		router.route("/business/*").handler(handler);
 
 		server.requestHandler(router::accept).listen(8080);
 	}
+	
+//	router.route("/gen").blockingHandler(routingContext -> {
+//	JsonObject config = new JsonObject() 
+//			.put("driver_class", "com.mysql.jdbc.Driver")
+//			.put("url", "jdbc:mysql://localhost/testdb")
+//			.put("user", "vertx")
+//			.put("password", "password");
+//	JDBCClient client = JDBCClient.createShared(vertx, config);
+//	JDBCAuth authProvider = JDBCAuth.create(vertx, client);
+//	String salt = authProvider.generateSalt();
+//	String hash = authProvider.computeHash("213231", salt);
+//	
+//	client.getConnection(res -> {
+//		if (res.succeeded()) {
+//			res.result().updateWithParams("INSERT INTO user VALUES (?, ?, ?)", new JsonArray().add("streetpoet").add(hash).add(salt), r -> {
+//				if (r.succeeded()) {
+//					System.out.println("insert success!");
+//				}else {
+//					System.out.println(r.cause().getMessage());
+//				}
+//				routingContext.response().end();
+//			});
+//		}
+//	});
+//});
 }
